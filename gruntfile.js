a9b29e74e5f280260bc412e8a4d569beaf41a99e
Preston Edmands <pedmands@gmail.com>
2015-08-14 22:31:40 -0400

    Added sass and watch tasks

--- gruntfile.js
+++ gruntfile.js
@@ -5,16 +5,44 @@ module.exports = function(grunt) {
 		concat : {
 			options: {
 				separator: '\n\n//-----------------------------------\n',
-				banner: '\n\n//------------------SCRIPT-----------------\n'
-			},
+				banner: '\n\n//------------------[SCRIPT]-----------------\n'
+			}, //options
 			dist : {
 				src: ['components/scripts/*.js'],
 				dest: 'builds/development/js/script.js'
-			}
-		}
+			} //dist
+		}, // :concat:
+		
+		sass : {
+			dist: {
+				options: {
+					style: 'expanded'
+				}, //options
+				files : [{
+					src: 'components/sass/style.scss',
+					dest: 'builds/development/css/style.css'
+				}] //files
+			} //dist
+		}, // :sass:
+		
+		watch: {
+			options: {
+				spawn: false
+			}, //options
+			scripts: {
+				files: ['builds/development/**/*.html',
+							'components/scripts/**/*.js',
+							'components/sass/**/*.scss'],
+				tasks: ['concat', 'sass']
+			} //scripts
+		} // :watch:
+		
 	}); //initConfig
 	
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.registerTask('default', ['concat']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default', ['concat', 'sass', 'watch']);
 	
 }; //wrapper function
